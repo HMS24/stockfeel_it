59cc171b5e4c77d159978e40094c1bdd2c817403
chenrohua24 <z7707092003@gmail.com>
2022-11-10 22:52:55 +0800

    feat: challenge 2 - create coffee table and migration

--- x_2/coffeesite/ilovecoffee/models.py
+++ x_2/coffeesite/ilovecoffee/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Coffee(models.Model):
+    """Model for representing a coffee"""
+
+    name = models.CharField(max_length=10)
+    bean_from = models.CharField(max_length=10)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        """String for representing the Model object."""
+
+        return f'{self.name}, {self.bean_from}, {self.price})'
+        
